b0258cd85982cb64e8e134d799c19565309d3355
Charles Muldoon <charm3728@gmail.com>
2019-12-26 14:13:13 -0800

    added pillars

--- src/board.js
+++ src/board.js
@@ -6,33 +6,45 @@ export class Board {
     this.populate()
   }
   _shuffle(arr) {
-  let curr = arr.length, temp, randIdx;
+    let curr = arr.length, temp, randIdx;
 
-  while (0 !== curr) {
+    while (0 !== curr) {
 
-    randIdx = Math.floor(Math.random() * curr);
-    curr -= 1;
+      randIdx = Math.floor(Math.random() * curr);
+      curr -= 1;
 
-    temp = arr[curr];
-    arr[curr] = arr[randIdx];
-    arr[randIdx] = temp;
+      temp = arr[curr];
+      arr[curr] = arr[randIdx];
+      arr[randIdx] = temp;
+    }
+    return arr;
   }
-
-  return arr;
+  change_spot(top, left){
+  
+    $(".spot").css({"top": top, "left": left})
   }
   populate(){
     let array = [1,2,3,4,5,6,7,8,9,10]
-    debugger
+    // debugger
     let shuffled = this._shuffle(array)
-    const section = $("<div>")
-    section.addClass("section")
-    this.$el.append(section)
-    // for(let i = 0; i < array.length; i++){
+    // const section = $("<div>")
+    // section.addClass("section")
+    // this.$el.append(section)
+    // // for(let i = 0; i < array.length; i++){
 
-    // }
-    // shuffled.map(height => {
-      
-    // })
+    // // }
+    // let spot = $("<div>")
+    // spot.addClass("spot")
+    // section.append(spot)
+
+    debugger
+    shuffled.map(height => {
+      const div = $("<div>")
+      div.addClass("spot")
+          .data("height", height)
+          .css("height",`${height * 40}px`)
+      $(".section").append(div)
+    })
 
 
   }
